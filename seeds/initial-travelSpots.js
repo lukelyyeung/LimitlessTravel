c52cf732bd67f81fda9accefc5b93aafa4253958
Thomas Lee <thomas.cylee@outlook.com>
2018-03-01 09:26:52 +0800

    modified some code

--- seeds/initial-travelSpots.js
+++ seeds/initial-travelSpots.js
@@ -13,13 +13,13 @@ exports.seed = function (knex, Promise) {
         // {airportCode: 'ICN', city: 'Seoul', country: 'South Korea', region: 'East Asia'},
         // { airportCode: 'RGN', city: 'Yangon', country: 'Myanmar', region: 'South East Asia' },
         // {airportCode: 'NYU', city: 'Bagan', country: 'Myanmar', region: 'South East Asia'},
-        // {airportCode: 'CNX', city: 'Chiang Mai', country: 'Thailand', region: 'South East Asia'},
+        {airportCode: 'CNX', city: 'Chiang Mai', country: 'Thailand', region: 'South East Asia', location: { lat: 18.7902778, long: 98.9816666 }},
         // {airportCode: 'VTE', city: 'Vientiane', country: 'Laos', region: 'South East Asia'},
-        // {airportCode: 'TLV', city: 'Jerusalem', country: 'Israel', region: 'Middle East'},
-        // {airportCode: 'BOM', city: 'Mumbai', country: 'India', region: 'Middle East'},
+        {airportCode: 'TLV', city: 'Jerusalem', country: 'Israel', region: 'Middle East', location: { lat: 31.7666667, long: 35.2333336 }},
+        {airportCode: 'BOM', city: 'Mumbai', country: 'India', region: 'Middle East', location: { lat: 19.0144100, long: 72.8479385 }},
         // {airportCode: 'BLR', city: 'Bangalore', country: 'India', region: 'Middle East'},
         // {airportCode: 'KEF', city: 'Reykjavik', country: 'Iceland', region: 'Europe'},
-        // {airportCode: 'HAM', city: 'Hamburg', country: 'Germany', region: 'Europe'},
+        {airportCode: 'HAM', city: 'Hamburg', country: 'Germany', region: 'Europe', location: { lat: 53.5500000, long: 10.0000000 }},
         { airportCode: 'SFO', city: 'San Francisco', country: 'USA', region: 'North America', location: { lat: 37.7749295, long: -122.4194183 } },
         // {airportCode: 'LAS', city: 'Las Vegas', country: 'USA', region: 'North America'}
       ]);
